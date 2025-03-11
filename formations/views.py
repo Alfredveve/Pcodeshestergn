88a6ea4add6b5578f4739f9f90c4a32de72cbbb9
Alfred <vevebea2022@outlook.fr>
2025-03-11 13:56:17 +0000

    repriseV2

--- formations/views.py
+++ formations/views.py
@@ -2,3 +2,4 @@ from django.shortcuts import render
 
 def fListeFormation(request):
     return render(request, 'formation/pListeFormation.html')
+
--- services/views.py
+++ services/views.py
@@ -32,33 +32,6 @@ def fsuccesService(request):
 
 
 
-"""
-def fInscriService(request, service_id=None):
-    # Récupérer le service sélectionné (si l'ID est passé en paramètre)
-    service = None
-    if service_id:
-        service = get_object_or_404(Service, id=service_id)
-
-    # Initialiser le formulaire avec le service pré-sélectionné
-    form = InscriServiceForm(request.POST or None, request.FILES or None, initial={'service': service} if service else None)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            # Enregistrer l'inscription
-            inscription = form.save(commit=False)
-            if service:
-                inscription.service = service  # Lier l'inscription au service sélectionné
-            inscription.save()
-            return redirect('serviceSucces')  # Redirection vers la page de succès
-        else:
-            # Afficher les erreurs de formulaire
-            messages.error(request, 'Erreur lors de l\'inscription. Veuillez vérifier les champs.')
-    
-    return render(request, 'service/inscriService.html', {'form': form})
-
-
-
-
 
 def fInscriService(request, service_id=None):
     # Récupérer le service sélectionné (si l'ID est passé en paramètre)
@@ -85,24 +58,4 @@ def fInscriService(request, service_id=None):
     
     return render(request, 'service/inscriService.html', {'form': form})
 
-    
-    """
-    
-# views.py
-def fInscriService(request, service_id=None):
-    service = get_object_or_404(Service, id=service_id)
-    
-    if request.method == 'POST':
-        form = InscriServiceForm(request.POST)
-        if form.is_valid():
-            inscription = form.save(commit=False)
-            inscription.service = service
-            inscription.save()
-            return redirect('serviceSucces')
-        else:
-            messages.error(request, 'Erreur lors de l\'inscription. Veuillez vérifier les champs.')
-            print(form.errors)
-    else:
-        form = InscriServiceForm(initial={'service': service})
-    
-    return render(request, 'service/inscriService.html', {'form': form, 'service': service})
+  
